36903355747cb12a9cd9dd75a0985a119e9fca72
lm002003 <lm002003@163.com>
2019-02-19 16:55:44 +0800

    v20190219

--- pytest/demo.py
+++ pytest/demo.py
@@ -1,3 +1,15 @@
+
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@File    :   test.py
+@Time    :   2019/02/15 08:13:51
+@Author  :   John Ederson 
+@Version :   1.0
+@Contact :   lm002003@163.com
+@License :   (C)Copyright 2018-2019, Liugroup-NLPR-CASIA
+@Desc    :   None
+'''
 import os
 import sys
 
