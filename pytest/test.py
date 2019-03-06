90c2717356eccf53ed3ee7be3693f2035bdd82f7
lm002003 <lm002003@163.com>
2019-03-06 17:15:53 +0800

    v20190306

--- pytest/test.py
+++ pytest/test.py
@@ -1,16 +1,25 @@
+
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- encoding: utf-8 -*-
 '''
 @File    :   test.py
-@Time    :   2019/02/15 08:13:51
+@Time    :   2019/03/06 10:22:09
 @Author  :   John Ederson 
 @Version :   1.0
 @Contact :   lm002003@163.com
 @License :   (C)Copyright 2018-2019, Liugroup-NLPR-CASIA
 @Desc    :   None
 '''
+import sys
+# here put the import lib
+# print("python:",sys.version)
+# print("hello python")
+# print('hello c++')
+a_dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
 
-import os
-print(os.getcwd())
-print("你好，python!")
-print(os.getpid())
+raw="Do you love Canglaoshi? Canglaoshi is a good teacher."
+raw_list=raw.split(" ")
+
+squares=[x**2 for x in range(1,10)]
+print(list(enumerate(raw_list)))
+print(squares)
