d326c8b3ae329225c18ddb711b21aa3006e4f1f2
lm002003 <lm002003@163.com>
2019-03-11 22:19:58 +0800

    v20190311.a

--- pytest/test.py
+++ pytest/test.py
@@ -10,23 +10,32 @@
 '''
 
 import os
-import io
+import sys
 
-# print(os.getcwd())
+print(os.getcwd())
 
 # os.chdir("pytest")  # （表示当前目录时，也可以去掉“./”，直接写文件名或者下级目录）---【中文注释需要在文件`首行`注明编码】
 # print(os.getcwd())
 # fi = open("demo.py")  # 单引号或双引号【print函数、块注释中单双引号都可以】
 # fl = open("d:\shiyan.py")
-fl = open("d:\\shiyan.py")
+fl = open("pytest\demo.py")
 # help(io.TextIOWrapper)
 # print(dir(__builtins__))
-print(fl)
 # print("this is my first program")
 # while True:
 #     line = fl.readline()
 #     n= 1
 #     print("------------",n+1)
 #     print(line)
-
-print(type(fl.readline()))
+n = 1
+b = []
+while True:
+    # print("================>第", n, "次")
+    n = n + 1
+    line = fl.readline()
+    if not line:
+        break
+    # b.append(line)
+    print(line, "")
+fl.close()
+# print(b)
